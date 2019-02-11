94864b22b4e3500cbbbf85b36e3d6dc8f49635e2
Mohammed <m.m.akinbayo@gmail.com>
2019-02-11 18:41:52 -0500

    Made a few unimportant changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -102,10 +102,7 @@ public class Robot extends TimedRobot {
   /**
    * This function is called periodically during operator control.
    */
-  @Override
-  public void teleopInit(){
-    ChomCheck.GoTest();
-  }
+
 
   @Override
   public void teleopPeriodic() {
