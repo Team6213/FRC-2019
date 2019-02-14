c93a49695151005783e2b96447fd254c1fb29b76
Ken <46426544+kenhassomething@users.noreply.github.com>
2019-02-14 15:17:05 -0500

    nothing really

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,25 +1,42 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------*/                                                                            
+/*            66666666       222222222222222         1111111       333333333333333       */
+/*           6::::::6       2:::::::::::::::22      1::::::1      3:::::::::::::::33     */
+/*          6::::::6        2::::::222222:::::2    1:::::::1      3::::::33333::::::3    */
+/*         6::::::6         2222222     2:::::2    111:::::1      3333333     3:::::3    */
+/*        6::::::6                      2:::::2       1::::1                  3:::::3    */
+/*       6::::::6                       2:::::2       1::::1                  3:::::3    */
+/*      6::::::6                     2222::::2        1::::1          33333333:::::3     */
+/*     6::::::::66666           22222::::::22         1::::l          3:::::::::::3      */
+/*    6::::::::::::::66       22::::::::222           1::::l          33333333:::::3     */
+/*    6::::::66666:::::6     2:::::22222              1::::l                  3:::::3    */
+/*    6:::::6     6:::::6   2:::::2                   1::::l                  3:::::3    */
+/*    6:::::6     6:::::6   2:::::2                   1::::l                  3:::::3    */
+/*    6::::::66666::::::6   2:::::2       222222   111::::::111   3333333     3:::::3    */
+/*     66:::::::::::::66    2::::::2222222:::::2   1::::::::::1   3::::::33333::::::3    */
+/*       66:::::::::66      2::::::::::::::::::2   1::::::::::1   3:::::::::::::::33     */
+/*         666666666        22222222222222222222   111111111111    333333333333333       */
+/*                                                                                    ©  */
+/*---------------------------------------------------------------------------------------*/
 
 package frc.robot;
 
+////////////////////////Custom imports////////////////////////////////
+import frc.pneumatics.Pneumatics;
+import frc.autonomous.Auto;
+import frc.vision.*;
+
+/////////////////////////WPILib imports///////////////////////////////
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -29,15 +46,32 @@ import edu.wpi.first.wpilibj.CameraServer;
  * project.
  */
 public class Robot extends TimedRobot {
+  
+  //Lightning McQueen///
   double speedMod;
   double Speed;
-
   private final DifferentialDrive robotDrive
       = new DifferentialDrive(new Spark(0), new Spark(1));
-  private final XboxController m_Xbox = new XboxController(0);
-  private final Timer timer = new Timer();
-  private Pneumatics ChomCheck = new Pneumatics();
+  //////////////////////
 
+  //PS74 Controller//
+  private final XboxController m_Xbox = new XboxController(0);
+  double rTrigger;
+  double lTrigger;
+  boolean rBumper;
+  boolean lBumper;
+  double lAnalog;
+  double rAnalog;
+  ///////////////////
+
+  //Avatar: The Last Airpods//
+  private Pneumatics ChomCheck = new Pneumatics();
+  //////////
+
+  //Bomb//
+  private final Timer timer = new Timer();
+  ////////
+  
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
@@ -68,17 +102,6 @@ public class Robot extends TimedRobot {
   public void robotPeriodic() {
   }
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
-   */
   @Override
   public void autonomousInit() {
     m_autoSelected = m_chooser.getSelected();
@@ -105,26 +128,26 @@ public class Robot extends TimedRobot {
   /**
    * This function is called periodically during operator control.
    */
+
+
   @Override
   public void teleopInit(){
-    //ChomCheck.GoTest();
-    ChomCheck.cOn();
+    rTrigger = m_Xbox.getRawAxis(3);
+    lTrigger = m_Xbox.getRawAxis(2);
+    rBumper = m_Xbox.getBumper(Hand.kRight);
+    lBumper = m_Xbox.getBumper(Hand.kLeft);
+    lAnalog = m_Xbox.getRawAxis(0);
+    //rAnalog = m_Xbox.getRawAxis();
   }
 
   @Override
   public void teleopPeriodic() {
-    double rTrigger = m_Xbox.getRawAxis(3);
-    double lTrigger = m_Xbox.getRawAxis(2);
-    double lAnalog = m_Xbox.getRawAxis(0);
-
-    ChomCheck.cOn();
-    if (m_Xbox.getBumper(Hand.kLeft) == true){
+    if (lBumper){
       ChomCheck.pushUp();
     }else{
       ChomCheck.stayStill();
     }
-
-    if (m_Xbox.getBumper(Hand.kRight) == true){
+    if (m_Xbox.getBumper(Hand.kRight)){
       ChomCheck.goBack();
     }
     
@@ -132,15 +155,16 @@ public class Robot extends TimedRobot {
     speedMod = GetSpeed(m_Xbox);
 
     //Drives Robot
-    if (m_Xbox.getRawAxis(3) > 0){
+    if (rTrigger > 0){
       robotDrive.arcadeDrive(rTrigger * speedMod, lAnalog);
       System.out.println("Right trigger is pressed");
-    }else if (m_Xbox.getRawAxis(2) > 0){
-      robotDrive.arcadeDrive(lTrigger * (speedMod * -1), lAnalog);
+    }else if (lTrigger > 0){
+      robotDrive.arcadeDrive(lTrigger * speedMod * -1, lAnalog);
       System.out.println("Left trigger is pressed");
     }else{
-      robotDrive.arcadeDrive(0, lAnalog * speedMod);
+      robotDrive.arcadeDrive(0, lAnalog);
     }
+
   }
 
   /**
@@ -166,14 +190,14 @@ public class Robot extends TimedRobot {
       Speed = 0.75;
       System.out.println("B is pushed");
     } 
-    /*if(yButton) {
+    if(yButton) {
       Speed = 1.0;
       System.out.println("Y is pushed");
     }
-    /*if(xButton){
+    if(xButton){
       Speed = 0.0;
       System.out.println("X is pushed");
-    }*/
+    }
 
     return Speed;
   }
